36586c5e0f1eac3a47e963390c27204f1cb2d0b4
quentinL52 <158512396+quentinL52@users.noreply.github.com>
2025-07-19 01:42:17 +0200

    Update Navbar.jsx

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -1,16 +1,12 @@
-import logo from '../../assets/AIRH_logo.png';
-
 function Navbar() {
     return (
         <nav className="navbar">
             <div className="container">
-                {/* Logo à gauche */}
                 <div className="navbar-brand">
-                    <img src={logo} alt="AIrh Logo" className="logo-icon" />
+                    <img src="/images/AIRH_logo.png" alt="AIrh Logo" className="logo-icon" />
                 </div>
                 
-                {/* Bouton CTA à droite */}
-                <a href="login" className="cta-button">
+                <a href="/login" className="cta-button">
                     <i className="fas fa-sign-in-alt"></i>
                     Se connecter
                 </a>
