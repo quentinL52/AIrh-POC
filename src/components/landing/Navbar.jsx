15fd0bfb90c1c40d1ed91c35fcac097bf226dad1
quentinL52 <158512396+quentinL52@users.noreply.github.com>
2025-07-19 01:53:39 +0200

    Update Navbar.jsx

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
         <nav className="navbar">
             <div className="container">
                 <div className="navbar-brand">
-                    <img src="/images/AIRH_logo.png" alt="AIrh Logo" className="logo-icon" />
+                    <img src="/images/AIrh_logo.png" alt="AIrh Logo" className="logo-icon" />
                 </div>
                 
                 <a href="/login" className="cta-button">
