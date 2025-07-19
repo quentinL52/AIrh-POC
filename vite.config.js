953184ecacfa7d69f5e557f118c06fa64c5aefa1
quentinL52 <158512396+quentinL52@users.noreply.github.com>
2025-07-19 20:15:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  }
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom']
+        }
+      }
+    }
+  },
+  base: './',
+  publicDir: 'public'
 })
